20e1adf2a08764db45ac35c731c8445e17208793
BatyutenkoYuriy <yurabatutenko07.09@gmail.com>
2025-09-27 19:08:51 +0300

    initial commit

--- Laba2/Laba2.java
+++ Laba2/Laba2.java
@@ -1,19 +1,37 @@
+import static java.lang.Math.*;
+
 public class Laba2 {
     public static void main(String[] args) {
         // Завдання 1: a = -1.49, b = 23.4, c = 1.23, d = 2.542
-        double a1 = -1.49, b1 = 23.4, c1 = 1.23, d1 = 2.542;
-        double y1 = Math.sqrt(Math.abs(Math.sin(a1) - (4 * Math.log(b1)) / Math.pow(c1, d1)));
-        System.out.println("Завдання 1: y = " + y1);
-
+        double result1 = calculateTask1(-1.49, 23.4, 1.23, 2.542);
+        
         // Завдання 2: a = 2.34, b = 0.756, c = 2.23, d = -1.653
-        double a2 = 2.34, b2 = 0.756, c2 = 2.23, d2 = -1.653;
-        double y2 = (Math.pow(Math.E, a2) + 3 * (Math.log10(c2))) / Math.sqrt(Math.pow(b2, c2));
-        y2 *= Math.abs(Math.atan(d2));
-        System.out.println("Завдання 2: y = " + y2);
-
+        double result2 = calculateTask2(2.34, 0.756, 2.23, -1.653);
+        
         // Завдання 3: a = 1.234, b = -3.12, c = 5.45, d = 2.0
-        double a3 = 1.234, b3 = -3.12, c3 = 5.45, d3 = 2.0;
-        double y3 = Math.pow((2 * Math.sin(a3) + Math.cos(b3 * Math.sqrt(c3))), d3);
-        System.out.println("Завдання 3: y = " + y3);
+        double result3 = calculateTask3(1.234, -3.12, 5.45, 2.0);
+
+        System.out.println("Завдання 1: y = " + result1 + "\n");
+        System.out.println("Завдання 2: y = " + result2 + "\n");
+        System.out.println("Завдання 3: y = " + result3 + "\n");
+    }
+
+    public static double calculateTask1(double a, double b, double c, double d) {
+        // y = √|sin(a) - (4 * ln(b)) / c^d|
+        double result = sqrt(abs(sin(a) - (4 * log(b)) / pow(c, d)));
+        return result;
+    }
+
+    public static double calculateTask2(double a, double b, double c, double d) {
+        // y = (e^a + 3 * lg(c)) / √(b^c) * |arctan(d)|
+        double result = (exp(a) + 3 * log10(c)) / sqrt(pow(b, c));
+        result *= abs(atan(d));
+        return result;
+    }
+
+    public static double calculateTask3(double a, double b, double c, double d) {
+        // y = (2 * sin(a) + cos(b * √c))^d
+        double result = pow((2 * sin(a) + cos(b * sqrt(c))), d);
+        return result;
     }
 }
